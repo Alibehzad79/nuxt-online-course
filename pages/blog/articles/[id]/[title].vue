30b1733ceeb77bf0fb39ab7e65bd83834a757049
Alibehzad79 <Alibehzad13791122@gmail.com>
2024-07-11 20:17:00 +0330

    add comments in blog detail

--- pages/blog/articles/[id]/[title].vue
+++ pages/blog/articles/[id]/[title].vue
@@ -14,6 +14,7 @@ const home = ref({
 });
 
 const { data: article, pending, error } = await useFetch(`https://freetestapi.com/api/v1/posts/${route.params.id}`)
+const { data: comments, cPending, cRefresh } = await useFetch('https://jsonplaceholder.typicode.com/comments', { lazy: true })
 const subEmail = ref('')
 const search = ref('')
 const btnLoading = ref(false)
@@ -35,6 +36,45 @@ const getSub = () => {
     }
 }
 
+const getAuthor = (author) => {
+    return navigateTo('/teachers/' + author)
+}
+
+const formData = {
+    fName: "",
+    lName: "",
+    email: "",
+    comment: "",
+}
+
+const commentLoading = ref(false)
+const toast = useToast();
+const sendComment = async () => {
+    if (formData) {
+        commentLoading.value = true
+        const { data: status } = await useFetch('https://jsonplaceholder.typicode.com/comments', {
+            method: "POST",
+            body: JSON.stringify(formData),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+        if (status.value === 'success') {
+            toast.add({ severity: 'success', summary: 'Success', detail: 'Comment Send Successfuly', life: 3000 })
+            cRefresh()
+            formData = {
+                fName: "",
+                lName: "",
+                email: "",
+                comment: "",
+            }
+        } else {
+            toast.add({ severity: 'error', summary: 'Error', detail: 'Failed to sending comment', life: 3000 })
+        }
+        commentLoading.value = false
+    }
+}
+
 </script>
 
 <template>
@@ -43,6 +83,7 @@ const getSub = () => {
             <Progress />
         </div>
         <div v-if="!load" class="flex flex-col gap-5">
+            <Toast sticky />
             <MyBreadcrumb :title="article.title" :home="home" />
             <div class="flex flex-col lg:flex-row gap-5">
                 <div v-if="pending">
@@ -53,30 +94,13 @@ const getSub = () => {
                     <Button type="button" severity="contrast" label="Refresh Data" icon="pi pi-refresh"
                         :loading="isRefresh" @click="refreshCourse" />
                 </div>
-                <div class="w-full lg:w-2/5 flex flex-col gap-5">
-                    <div class="w-full flex p-5">
-                        <InputText class="w-full rounded-none" type="text" v-model="search" placeholder="Search..."
-                            size="large" />
-                        <Button @click="getSearch" type="button" icon="pi pi-search" severity="info"
-                            class="rounded-none" :loading="btnLoading" :disabled="search.value != null" size="large" />
-                    </div>
-                    <div class="bg-red-500 text-white p-5 flex flex-col gap-5 rounded-xl">
-                        <h5 class="font-bold text-xl">Subscribe Notifications</h5>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea tenetur eaque atque quos fugiat
-                            ducimus facilis accusantium, temporibus dolorum officiis. Similique voluptatum est animi,
-                            amet aut vel consequatur tempora odit!</p>
-                        <InputText type="email" size="large" placeholder="Email" v-model="subEmail" class="" />
-                        <Button @click="getSub" type="button" label="Subscribe" severity="info" class="rounded-full"
-                            :loading="subLoading" :disabled="subEmail.value != null" size="large" />
-                    </div>
-                </div>
                 <div class="w-full lg:w-4/5 p-5 flex flex-col gap-5">
                     <Image src="/course.jpg" width="100%" />
                     <div>
                         {{ article.content }}
                     </div>
                     <div class="flex flex-col gap-2">
-                        <h5 class="font-bold text-2xl">Share in:</h5>
+                        <h5 class="font-bold text-xl">Share in:</h5>
                         <div class="flex felx-wrap gap-2">
                             <NuxtLink to="#">
                                 <Button label="Telegram" icon="pi pi-telegram" severity="secondary"
@@ -95,6 +119,73 @@ const getSub = () => {
                                     class="bg-green-500 text-white" />
                             </NuxtLink>
                         </div>
+                    </div>
+                    <div class="flex flex-col gap-5 bg[#fdfcff] mt-5">
+                        <div>
+                            <span class="font-bold text-xl">About Author</span>
+                            <Divider />
+                        </div>
+                        <div class="flex gap-5 cursor-pointer" @click="getAuthor(article.author.replaceAll(' ', '-'))">
+                            <Avatar image="/image.jpg" class="mr-2 min-w-20 min-h-20" size="xlarge" shape="circle" />
+                            <div>
+                                <strong>{{ article.author }} <i class="pi pi-link"></i></strong>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil animi libero, quisquam
+                                    officia accusamus porro. Commodi minus cumque saepe ea quidem sequi ab eius
+                                    repellendus! Tempore dignissimos voluptate cumque magni?</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex flex-col gap-5 mt-5">
+                        <h6 class="font-bold text-xl">Write a Comment <i class="pi pi-comment"></i></h6>
+                        <form class="flex flex-col gap-5" @submit.prevent="sendComment">
+                            <div class="flex flex-col md:flex-row justify-between gap-5">
+                                <InputText v-model="formData.fName" type="text" placeholder="Name" class="w-full"
+                                    size="large" required />
+                                <InputText v-model="formData.lName" type="text" placeholder="Name" class="w-full"
+                                    size="large" required />
+                            </div>
+                            <InputText v-model="formData.email" type="email" size="large" placeholder="E-mail"
+                                class="w-full" required />
+                            <Textarea v-model="formData.comment" placeholder="Your Comment" rows="5" cols="30"
+                                required />
+                            <Button type="submit" label="Send" :loading="commentLoading" severity="info" />
+                        </form>
+
+                    </div>
+                    <div class="flex flex-col gap-5 bg[#fdfcff] mt-5">
+                        <div>
+                            <span class="font-bold text-xl">Comments <i class="pi pi-comments"></i></span>
+                            <Divider />
+                        </div>
+                        <div v-if="cPending">
+                            <Progress />
+                        </div>
+                        <div class="flex gap-5 cursor-pointer border p-5 rounded-xl" v-for="comment in comments"
+                            :key="comment.id">
+                            <Avatar :label="comment.name[0]" class="mr-2 min-w-20 min-h-20" size="xlarge"
+                                shape="circle" />
+                            <div>
+                                <strong>{{ comment.name }}</strong>
+                                <p>{{ comment.body }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full lg:w-2/5 flex flex-col gap-5">
+                    <div class="w-full flex p-5">
+                        <InputText class="w-full rounded-none" type="text" v-model="search" placeholder="Search..."
+                            size="large" />
+                        <Button @click="getSearch" type="button" icon="pi pi-search" severity="info"
+                            class="rounded-none" :loading="btnLoading" :disabled="search.value != null" size="large" />
+                    </div>
+                    <div class="bg-red-500 text-white p-5 flex flex-col gap-5 rounded-xl">
+                        <h5 class="font-bold text-xl">Subscribe Notifications</h5>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea tenetur eaque atque quos fugiat
+                            ducimus facilis accusantium, temporibus dolorum officiis. Similique voluptatum est animi,
+                            amet aut vel consequatur tempora odit!</p>
+                        <InputText type="email" size="large" placeholder="Email" v-model="subEmail" class="" />
+                        <Button @click="getSub" type="button" label="Subscribe" severity="info" class="rounded-full"
+                            :loading="subLoading" :disabled="subEmail.value != null" size="large" />
                     </div>
                 </div>
             </div>
